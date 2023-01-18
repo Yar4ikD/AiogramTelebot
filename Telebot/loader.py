028fa1f2dbc16d89a8551f6bd45d9f1cc367683a
Yaroslav Dmytrovskyi <dmiyaroslav92@gmail.com>
2023-01-18 11:49:39 +0300

    Add a doenv file

--- Telebot/loader.py
+++
@@ -1,6 +0,0 @@
-from config import TOKEN
-from aiogram import Bot, Dispatcher
-
-
-bot = Bot(token=TOKEN, parse_mode='HTML')
-dp = Dispatcher(bot)
--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
+from dotenv import load_dotenv, find_dotenv
 
-TOKEN = '5611221323:AAGFGlDYpICZznvTEfjXrf3sQCXryRcZp44'
-YANDEX_API_KEY = '23ca22c9-181e-4656-ae8a-c9c3f4c643b8'
+load_dotenv(find_dotenv())
+
+TOKEN = os.getenv('TOKEN')
+YANDEX_API_KEY = os.getenv('YANDEX_API_KEY')
+
 ALL_COUNTRIES_CODE_JSON_PATH = os.path.realpath('../database/all_countries_code.json')
