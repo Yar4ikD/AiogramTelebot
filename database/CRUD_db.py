a7285f74c3c21310037355f5281b3e1bdbfcfdef
Yaroslav Dmytrovskyi <dmiyaroslav92@gmail.com>
2023-01-30 22:03:13 +0300

    Created command bot - List of nearest stations

--- database/CRUD_db.py
+++ database/CRUD_db.py
@@ -1,14 +1,30 @@
 import sqlite3 as sq
+import aiosqlite
+from typing import List, Tuple, Optional
+from config import DB_PATH
 
 """
-Модуль для создания БД и работы с ней.
+Модуль для работы с БД.
 СУБД - SQLite3
 """
 
 
 class YandexDB:
 
-    def __init__(self, name_db: str = 'code_rus') -> None:
+    TABLE = 'rus'
+    REGION_TITLE = 'region_title'
+    REGION_CODES = 'region_codes'
+    TITLE_SETTLE = 'title_settle'
+    CODES_SETTLE = 'codes_settle'
+    DIRECTION = 'direction'
+    TITLE = 'title'
+    STATION_TYPE = 'station_type'
+    TRANSPORT_TYPE = 'transport_type'
+    YANDEX_CODE = 'yandex_code'
+    name_db = DB_PATH
+    base = None
+
+    def __init__(self, name_db: str = 'RusCode') -> None:
         """
         Магический метод класса, инициализирует БД.
         Создает БД с расширением db и производится подключение к конкретной БД.
@@ -25,6 +41,10 @@ class YandexDB:
         with sq.connect(self.name) as con:  # производится подключение к БД
             self.__cur = con.cursor()  # Создается курсор из соединения с БД
             self.__base = con  # соединения с БД
+
+    @classmethod
+    async def connect_db(cls) -> None:
+        cls.base = await aiosqlite.connect(cls.name_db)
 
     def add_table(self, table: str = 'user (?, ?, ?)') -> None:
         """
@@ -74,3 +94,53 @@ class YandexDB:
         query = f"DROP TABLE IF EXISTS {name_tb}"
         self.__cur.execute(query)
         self.__base.commit()
+
+    @classmethod
+    async def select_region_or_settlement(cls, user_msg: str, type_query: str, table: str = TABLE):
+
+        query = None
+        if type_query == 'region':
+
+            query = f"""SELECT {cls.REGION_CODES}, {cls.REGION_TITLE} FROM {table} 
+            WHERE {cls.REGION_TITLE} LIKE '{user_msg}%' OR {cls.REGION_TITLE} LIKE '%{user_msg}' 
+            """
+
+        elif type_query == 'settlement':
+            query = f"SELECT {cls.CODES_SETTLE}, {cls.TITLE_SETTLE} FROM {table} " \
+                    f"WHERE {cls.TITLE_SETTLE} LIKE '%{user_msg}%' OR {cls.DIRECTION} LIKE '%{user_msg}%' " \
+                    f"AND ({cls.STATION_TYPE} LIKE 'station' OR {cls.STATION_TYPE} LIKE 'train_station' " \
+                    f"OR {cls.STATION_TYPE} LIKE 'bus_station' OR {cls.STATION_TYPE} LIKE 'airport')"
+        else:
+            return None
+
+        cursor = await cls.base.execute(query)
+        row = await cursor.fetchone()
+        return row
+
+    @classmethod
+    async def select_list_station(cls, region_code: str, settlement_code: str, table: str = TABLE, limit=None):
+
+        query = f'SELECT {cls.TITLE}, {cls.STATION_TYPE}, {cls.TRANSPORT_TYPE} FROM {table} WHERE ' \
+                f'({cls.REGION_CODES} = "{region_code}" AND {cls.CODES_SETTLE} = "{settlement_code}") ' \
+                f'AND ({cls.STATION_TYPE} LIKE "station" OR {cls.STATION_TYPE} LIKE "train_station" ' \
+                f'OR {cls.STATION_TYPE} LIKE "bus_station" OR {cls.STATION_TYPE} LIKE "airport")'
+
+        cursor = await cls.base.execute(query)
+        rows = await cursor.fetchmany(size=15)
+
+        return rows
+
+    @classmethod
+    async def select_yandex_code(cls, user_msg: str, region: str, settlement: str, table: str = TABLE, limit: int = 10):
+
+        query = f"""
+        SELECT {cls.YANDEX_CODE}, {cls.TITLE} FROM {table} WHERE {cls.REGION_CODES} LIKE '{region}' 
+        AND {cls.CODES_SETTLE} LIKE '{settlement}' AND {cls.TITLE} LIKE '%{user_msg}%' 
+        LIMIT {limit}
+        """
+
+        cursor = await cls.base.execute(query)
+        row = await cursor.fetchone()
+        print(row)
+        return row
+
--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from aiogram import executor
 from loader import dp
-from commands import start
+from commands import start, station_timetable, list_nearest_stations
+from database.CRUD_db import YandexDB
 
 
 async def on_start(_) -> None:
-    """ Служебная функция, выводит на экран консоли сообщения, что бот работает. """
+    """ Функция, выводит на экран консоли сообщения, что бот работает.
+        Установляивет соединения с БД.
+        Запускает функции, для регистрации обработчиков команд ТГ-бота
+    """
     print('Бот начал работать')
+    await YandexDB.connect_db()
+    await start.register_command_start(dp)
+    await station_timetable.register_command_timetable(dp)
+    await list_nearest_stations.register_command_nearest_station(dp)
 
-
-start.register_command_start(dp)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, on_startup=on_start)
